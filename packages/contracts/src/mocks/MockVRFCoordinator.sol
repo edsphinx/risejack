1ddb9e7c9f1392630661c0d2d06fa89ddee4137e
Oscar Fonseca <edsphinx@gmail.com>
2025-12-29 04:12:57 -0600

    style: apply CI-compatible code formatting

--- packages/contracts/src/mocks/MockVRFCoordinator.sol
+++ packages/contracts/src/mocks/MockVRFCoordinator.sol
@@ -74,7 +74,10 @@ contract MockVRFCoordinator is IVRFCoordinator {
      * @param requestId The request ID to fulfill
      * @param randomNumbers Array of random numbers to send to consumer
      */
-    function fulfillRequest(uint256 requestId, uint256[] calldata randomNumbers) external {
+    function fulfillRequest(
+        uint256 requestId,
+        uint256[] calldata randomNumbers
+    ) external {
         address consumer = requestToConsumer[requestId];
         require(consumer != address(0), "Request ID not found");
         require(!requestFulfilled[requestId], "Already fulfilled");
@@ -94,7 +97,10 @@ contract MockVRFCoordinator is IVRFCoordinator {
      * @param requestId The request ID to fulfill
      * @param seed Seed to generate random numbers from
      */
-    function fulfillRequestWithSeed(uint256 requestId, uint256 seed) external {
+    function fulfillRequestWithSeed(
+        uint256 requestId,
+        uint256 seed
+    ) external {
         address consumer = requestToConsumer[requestId];
         require(consumer != address(0), "Request ID not found");
         require(!requestFulfilled[requestId], "Already fulfilled");
